ecaef9b12ad2c08883b251a05df4b4ee37b88440
msk007 <krawiec515@gmail.com>
2015-09-09 20:24:26 -0400

    added things
    set up framework for program

---
+++ linear_algebra.py
@@ -0,0 +1,28 @@
+class ShapeException(Exception):
+    pass
+
+def shape(vector)
+
+
+
+def add_vector(vector1,vector2)
+
+
+
+def vector_sum():
+
+
+
+def dot(vector1,vector2)
+
+
+
+def vector_multiply(vector, scalar)
+
+
+
+def vector_mean():
+
+
+
+def magnitude(vector):
