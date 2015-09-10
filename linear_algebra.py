5df7e82f499c05cc54793ae4e7cde929bd4f48ac
msk007 <krawiec515@gmail.com>
2015-09-09 22:28:29 -0400

    added

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,15 +1,23 @@
 class ShapeException(Exception):
     pass
 
-def shape(vector)
+def shape(vector):
+    if type(vector[0]) != list:
+        return (len(vector),)
+    return (len(vector),len(vector[0]))
 
 
 
-def add_vector(vector1,vector2)
-
+def add_vector(vector1,vector2):
+    raise_exception(vector1, vector2)
+    addition = [x + y for x, y in zip(vector1, vector2)]
+    return addition
 
 
 def vector_sum():
+    raise_exception(vector1, vector2)
+    subtraction = [x - y for x, y in zip(vector1, vector2)]
+    return subtraction
 
 
 
