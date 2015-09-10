808559af71105b8add40572a48c6cc3883f6bcf3
msk007 <krawiec515@gmail.com>
2015-09-09 22:44:34 -0400

    update

--- linear_algebra.py
+++ linear_algebra.py
@@ -16,7 +16,7 @@ def add_vector(vector1,vector2):
 
 def vector_sub():
     raise_exception(vector1, vector2)
-    subtraction = [x - y for x, y in zip(vector1, vector2)]
+    subtraction =[x - y for x, y in zip(vector1, vector2)]
     return subtraction
 
 
@@ -30,7 +30,14 @@ def vector_multiply(vector, scalar)
 
 
 def vector_mean():
-
+    num = len(args)
+    vector = vector_sum(*args)
+    mean = [x/num for x in vector]
+    return mean
 
 
 def magnitude(vector):
+    exp = [x**2 for x in vector]
+    sum_exp = sum(exp)
+    magnitude = math.sqrt(sum_exp)
+    return magnitude
